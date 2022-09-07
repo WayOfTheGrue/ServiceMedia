bfe47f7bbfa657439a06ee636a42dba6d27eba04
Austin Way <austinwaydesign@gmail.com>
2022-09-06 19:05:17 -0500

    Changes to Oculus Validate Route
    1. Request console.log call moved to the top of the function to not be culled by the app and action checks.
    2. Action check moved to before app check and inverted to reduce redundant code.
    3. App check changed to a switch. This more easily allows values to be added later.
    4. App check only modifies the data.access_token value to reduce redundant code and more clearly mark what the switch does.
    5. Removed the string at the beginning of the resulting response. This makes sure that the client does not need to search the string for '{' in order to parse the JSON result object.
    6. Error result strings cleaned up.
    
    These changes are untested and need to be tested.

--- routes/oculus_routes.js
+++ routes/oculus_routes.js
@@ -2,51 +2,48 @@ const express = require("express");
 const oculus_router = express.Router();
 const axios =  require("axios");
 
-oculus_router.post("/:app/:action", function (req, res) {
-    
-    if (req.params.app.toString().toLowerCase() == "cb_rift") {
-        console.log("oculus request for cowbots " + JSON.stringify(req.body));
-        if (req.params.action.toString().toLowerCase() == "validate") {
-            let data = {};
-            data.access_token = process.env.CB_OCULUS_RIFT_TOKEN;
-            data.nonce = req.body.nonce;
-            data.user_id = req.body.oID;
-            console.log(JSON.stringify(data));
-            axios.post("https://graph.oculus.com/user_nonce_validate/", data) 
-            .then((response) => {
-                // console.log("oculus api validaaqtion response: " + JSON.stringify(response.data));
-                res.send("nonce validation response: " + JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                res.send(error);
-            })
-        } else {
-            res.send("dunno...no action");
-        }
-    } else if ((req.params.app.toString().toLowerCase() == "cb_quest")) {
-        if (req.params.action.toString().toLowerCase() == "validate") {
-            let data = {};
-            data.access_token = process.env.CB_OCULUS_QUEST_TOKEN;
-            data.nonce = req.body.nonce;
-            data.user_id = req.body.oID;
-            console.log(JSON.stringify(data));
-            axios.post("https://graph.oculus.com/user_nonce_validate/", data)
-            .then((response) => {
-                // console.log("oculus api validaaqtion response: " + JSON.stringify(response.data));
-                res.send("nonce validation response: " + JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                res.send(error);
-            })
-        } else {
-            res.send("dunno...no action");
-        }
-    } else {
-        res.send("for who/what?");
+oculus_router.post("/:app/:action", function (req, res) 
+{
+    console.log("oculus request " + JSON.stringify(req.body));
+    if (req.params.action.toString().toLowerCase() != "validate") 
+    {
+        res.send("No Action");
+        return;
     }
+    
+    let data = {};
+    switch(req.params.app.toString().toLowerCase())
+    {
+        case "cb_rift":
+            data.access_token = process.env.CB_OCULUS_RIFT_TOKEN;
+        break;
+            
+        case "cb_quest":
+            data.access_token = process.env.CB_OCULUS_QUEST_TOKEN;
+        break;
+            
+        default:
+            res.send("Invalid App");
+        return;
+    
+    }
+    
+    data.nonce = req.body.nonce;
+    data.user_id = req.body.oID;
+    console.log(JSON.stringify(data));
+    axios.post("https://graph.oculus.com/user_nonce_validate/", data) 
+    .then((response) => 
+    {
+        res.send(response);
+    })
+    .catch(function (error) 
+    {
+        res.send(error);
+    })
 });
 
-    oculus_router.get("/test", function (req, res) {
+    oculus_router.get("/test", function (req, res) 
+    {
         res.send("OK!");
     });
 
